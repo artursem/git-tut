9f95ce42a915115e0178273c585beb2ccc45f235
artursem <artur.semeniuk@gmail.com>
2021-04-25 19:12:45 +0200

    fix js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const title = document.querySelector('h1');
 const hero = document.querySelector('.hero');
+hero.classList.add('url1');
 title.addEventListener('click', () => {
-    hero.style.backgroundImage='url(surf.jpeg)';
+    if (hero.classList.contains('url1')) {
+        hero.classList.add('url2');
+        hero.classList.remove('url1');
+        title.innerText = 'surf';
+    } else {
+        hero.classList.add('url1');
+        hero.classList.remove('url2');
+        title.innerText = 'baloons';
+    }
 })
